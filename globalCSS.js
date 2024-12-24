2b57ff7b7e2fd97411f22b40bd85afd4e5f3b2af
Cauê Alves <cauealvesb4@gmail.com>
2024-12-24 15:30:23 -0300

    Criando cores dos temas e criando css global

---
+++ globalCSS.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalCSS = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-size: 62.5%
+    }
+`;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components"
-import { theme } from "../../theme"
+import { theme } from "../theme"
 
 export function App() {
 
