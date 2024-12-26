90c97eec8f6f8d3e01d7dfb4d5c6ddccda80ef76
Cauê Alves <cauealvesb4@gmail.com>
2024-12-25 21:42:44 -0300

    chore: Mudando o tema do projeto

--- theme.js
+++ theme.js
@@ -1,17 +1,9 @@
 export const theme = {
-    '--blue-900': '#003366',
-    '--blue-800': '#004080',
-    '--blue-700': '#00509E',
-    '--blue-600': '#0066CC',
-    '--blue-500': '#007FFF',
-    '--blue-400': '#3399FF',
-    '--blue-300': '#66B2FF',
-    '--blue-200': '#99CCFF',
-    '--blue-100': '#CCE5FF',
+    '--blue-principal': '#007FFF',
+    '--blue-secound': '#9CCEFF',
 
-    '--red-500': '#FF0000',
-    '--red-300': '#FF4D4D',
+    '--red-principal': '#FC736E',
+    '--red-secound': '#FFC3C1',
 
-    '--orange-500': '#FF8C00',
-    '--orange-300': '#FFA500',
+    '--gray-principal': '#CFCFCF'
 };
